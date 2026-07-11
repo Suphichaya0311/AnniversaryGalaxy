62d85aeec564d18889fe7ca9812e5ba95362090e
Suphichaya0311 <oil.0877611417@gmail.com>
2026-07-11 08:55:33 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,67 @@
+// ปุ่ม Enter
+const enterBtn = document.getElementById("enterBtn");
 
+if (enterBtn) {
+    enterBtn.addEventListener("click", () => {
+        document.getElementById("memory").scrollIntoView({
+            behavior: "smooth"
+        });
+
+        const music = document.getElementById("bgMusic");
+
+        if (music) {
+            music.play();
+        }
+    });
+}
+
+// Popup ดาว
+const stars = document.querySelectorAll(".star");
+const popup = document.getElementById("popup");
+const closePopup = document.getElementById("closePopup");
+
+stars.forEach(star => {
+    star.onclick = () => {
+        popup.style.display = "flex";
+    }
+});
+
+if(closePopup){
+    closePopup.onclick=()=>{
+        popup.style.display="none";
+    }
+}
+
+// ตุ๊กตา
+const bear=document.getElementById("bear");
+const bearPopup=document.getElementById("bearPopup");
+const closeBear=document.getElementById("closeBear");
+
+if(bear){
+    bear.onclick=()=>{
+        bearPopup.style.display="flex";
+    }
+}
+
+if(closeBear){
+    closeBear.onclick=()=>{
+        bearPopup.style.display="none";
+    }
+}
+
+// จดหมาย
+const openLetter=document.getElementById("openLetter");
+const letterPopup=document.getElementById("letterPopup");
+const closeLetter=document.getElementById("closeLetter");
+
+if(openLetter){
+    openLetter.onclick=()=>{
+        letterPopup.style.display="flex";
+    }
+}
+
+if(closeLetter){
+    closeLetter.onclick=()=>{
+        letterPopup.style.display="none";
+    }
+}
